99d7e4888f236cb67580c05088dfadcf75219a0c
Alyssa Gibson <amgibson@noctrl.edu>
2019-02-28 18:53:42 +0000

    SuperSales.cpp created online with Bitbucket

---
+++ SuperSales.cpp
@@ -0,0 +1,23 @@
+/* 
+ * File:   SuperSales.cpp
+ * Author: Alyssa Gibson
+ * 
+ * Created on February 26, 2019, 3:25 PM
+ */
+
+#include "SuperSales.h"
+#include <string>
+
+using namespace std;
+
+SuperSales::SuperSales(string t, string n, string sid, string bid):Sales(t, n, sid, bid){
+    commission = 0;
+}
+
+double SuperSales::getCommission() {
+    // Commission calculation goes here
+}
+
+SuperSales::~SuperSales() {
+}
+
