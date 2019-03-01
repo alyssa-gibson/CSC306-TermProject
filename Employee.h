45db581cac10912437589622934c20dcaf15942b
Dan Jirasek <dpjirasek@noctrl.edu>
2019-03-01 07:37:12 +0000

    Employee.h edited online with Bitbucket Changed private to protected so all derived classes would have access to the member in its functions for any operations.

--- Employee.h
+++ Employee.h
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /* 
  * File:   Employee.h
  * Author: Alyssa Gibson
@@ -17,30 +11,31 @@
 #include <string>
 
 class Employee {
-public:
-    Employee(std::string, std::string, std::string, std::string);
-    virtual double getCommission() = 0;
-    std::string getTitle(); // returns the Employee's title
-    std::string getName(); // returns the Employee's name
-    std::string getSalesID(); // returns the Employee's sale ID
-    std::string getBossID(); // returns the Employee's boss ID
-    
-private:
-    std::string title;
-    // Title: Whether this specific Employee is a:
-    //        - Sales Employee
-    //        - SuperSales Employee
-    //        - Supervisor Employee
-    //        - Manager Employee
-    std::string name;
-    // Name: The first and last name of the Employee.
-    std::string salesPersonID;
-    // Sales Person ID: The ID number of this specific individual.
-    std::string bossID;
-    // Boss ID: The ID number of the person who manages this specific
-    //          individual, if applicable.
-    
+    public:
+        Employee();
+        Employee(const Employee& orig);
+        Employee(std::string, std::string, std::string, std::string);
+        virtual double getCommission() = 0;
+        std::string getTitle(); // returns the Employee's title
+        std::string getName(); // returns the Employee's name
+        std::string getSalesID(); // returns the Employee's sale ID
+        std::string getBossID(); // returns the Employee's boss ID
+        std::string toString();// returns all of the Employee's data
+        virtual ~Employee();
+    protected:
+        std::string title;
+        // Title: Whether this specific Employee is a:
+        //        - Sales Employee
+        //        - SuperSales Employee
+        //        - Supervisor Employee
+        //        - Manager Employee
+        std::string name;
+        // Name: The first and last name of the Employee.
+        std::string salesPersonID;
+        // Sales Person ID: The ID number of this specific individual.
+        std::string bossID;
+        // Boss ID: The ID number of the person who manages this specific
+        //          individual, if applicable.    
 };
-
 #endif /* EMPLOYEE_H */
 
