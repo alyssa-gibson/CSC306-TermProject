14b6afc49942605999404edfe0df2997a8de554c
Dan Jirasek <dpjirasek@noctrl.edu>
2019-03-01 07:39:32 +0000

    Sales.cpp edited online with Bitbucket

--- Sales.cpp
+++ Sales.cpp
@@ -10,6 +10,17 @@
 
 using namespace std;
 
+Sales::Sales(){
+}
+
+Sales::Sales(const Sales& orig){
+    title = orig.title;
+    name = orig.name;
+    salesPersonID = orig.salesPersonID;
+    bossID = orig.bossID;
+    commission = orig.commission;
+}
+
 Sales::Sales(string t, string n, string sid, string bid):Employee(t, n , sid, bid) {
     commission = 0;
 }
@@ -18,6 +29,9 @@ double Sales::getCommission() {
     // Commission calculations go here.
 }
 
+bool Sales::operator <(const Sales& other){
+    return name < other.name;
+}
 Sales::~Sales() {
     
 }
