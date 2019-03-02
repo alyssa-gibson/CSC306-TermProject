792a7e80ae6a1a8dd478f6cc57695f274912fc0e
Ryan Ziolkowski <rjziolkowski@noctrl.edu>
2019-03-02 05:31:24 +0000

    SuperSales.cpp edited online with Bitbucket

--- SuperSales.cpp
+++ SuperSales.cpp
@@ -18,10 +18,32 @@ SuperSales::SuperSales(const SuperSales& orig){
 
 SuperSales::SuperSales(string t, string n, string sid, string bid):Sales(t, n, sid, bid){
     commission = 0;
+    grossSales = 0;
 }
 
-double SuperSales::getCommission() {
+string SuperSales::toString() {
+    char cSale[32], cCommission[32];
+    commission = getCommission();
+    sprintf(cSale, "%0.2f", grossSales);
+    sprintf(cCommission, "%0.2f", commission);
+    return "Super Sales: " + name + " Gross Sales: $" + cSale + " Commission: $" + cCommission;
+}
+
+double SuperSales::getCommission(){
     // Commission calculation goes here
+    return grossSales * 0.065;
+}
+
+double SuperSales::getSales() {
+    return grossSales;
+}
+
+void SuperSales::setCommission(double c) {
+    commission = c;
+}
+
+void SuperSales::setSales(double gS) {
+    grossSales = gS;
 }
 
 SuperSales::~SuperSales() {
