4c32c62c95d4e534cfd3a95e6fccfc4d91eee1d9
Ryan Ziolkowski <rjziolkowski@noctrl.edu>
2019-03-02 05:30:35 +0000

    Sales.cpp edited online with Bitbucket

--- Sales.cpp
+++ Sales.cpp
@@ -19,14 +19,37 @@ Sales::Sales(const Sales& orig){
     salesPersonID = orig.salesPersonID;
     bossID = orig.bossID;
     commission = orig.commission;
+    grossSales = orig.grossSales;
 }
 
 Sales::Sales(string t, string n, string sid, string bid):Employee(t, n , sid, bid) {
     commission = 0;
+    grossSales = 0;
+}
+
+string Sales::toString(){
+    char cSale[32], cCommission[32];
+    commission = getCommission();
+    sprintf(cSale, "%0.2f", grossSales);
+    sprintf(cCommission, "%0.2f", commission);
+    return "Sales: " + name + " Gross Sales: $" + cSale + " Commission: $" + cCommission;
 }
 
 double Sales::getCommission() {
     // Commission calculations go here.
+    return grossSales * 0.05;
+}
+
+double Sales::getSales() {
+    return grossSales;
+}
+
+void Sales::setCommission(double c){
+    commission = c;
+}
+
+void Sales::setSales(double gS) {
+    grossSales = gS;
 }
 
 bool Sales::operator <(const Sales& other){
