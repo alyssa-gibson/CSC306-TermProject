95bced14ba84f20d8eb139f66888a33480e83919
Ryan Ziolkowski <rjziolkowski@noctrl.edu>
2019-03-02 05:29:31 +0000

    Manager.cpp edited online with Bitbucket

--- Manager.cpp
+++ Manager.cpp
@@ -7,6 +7,7 @@
 
 #include "Manager.h"
 #include <string>
+
 using namespace std;
 
 Manager::Manager(){
@@ -17,11 +18,32 @@ Manager::Manager(const Manager& orig){
 
 Manager::Manager(string t, string n, string sid, string bid):SuperSales(t, n, sid, bid) {
     commission = 0;
+    grossSales = 0;
+}
+
+string Manager::toString() {
+    char cSale[32], cCommission[32];
+    commission = getCommission();
+    sprintf(cSale, "%0.2f", grossSales);
+    sprintf(cCommission, "%0.2f", commission);
+    return "Manager: " + name + " Gross Sales: $" + cSale + " Commission: $" + cCommission;
 }
 
 double Manager::getCommission() {
     // Commission calculation goes here
     return commission;
+}
+
+double Manager::getSales() {
+    return grossSales;
+}
+
+void Manager::setCommission(double c) {
+    commission = c;
+}
+
+void Manager::setSales(double gS)  {
+    grossSales = gS;
 }
 
 Manager::~Manager() {
