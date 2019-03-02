57256fa206732975309fcbaf8951636f008720af
Ryan Ziolkowski <rjziolkowski@noctrl.edu>
2019-03-02 05:32:16 +0000

    Supervisor.cpp edited online with Bitbucket

--- Supervisor.cpp
+++ Supervisor.cpp
@@ -20,10 +20,32 @@ Supervisor::Supervisor(const Supervisor& orig){
 Supervisor::Supervisor(string t, string n, string sid, string bid):Manager(t, n, sid, bid) 
 {
     commission = 0;
+    grossSales = 0;
+}
+
+string Supervisor::toString()  {
+    char cSale[32], cCommission[32];
+    commission = getCommission();
+    sprintf(cSale, "%0.2f", grossSales);
+    sprintf(cCommission, "%0.2f", commission);
+    return "Supervisor: " + name + " Gross Sales: $" + cSale + " Commission: $" + cCommission;
 }
 
 double Supervisor::getCommission(){
     // Commission calculations go here.
+    return commission;
+}
+
+double Supervisor::getSales() {
+    return grossSales;
+}
+
+void Supervisor::setCommission(double c) {
+    commission = c;
+}
+
+void Supervisor::setSales(double gS) {
+    grossSales = gS;
 }
 
 Supervisor::~Supervisor() {
