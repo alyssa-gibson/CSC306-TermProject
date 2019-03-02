808809b6b1df3c4b6d4eece7001cc53681e029a5
Ryan Ziolkowski <rjziolkowski@noctrl.edu>
2019-03-02 05:32:39 +0000

    Supervisor.h edited online with Bitbucket

--- Supervisor.h
+++ Supervisor.h
@@ -15,7 +15,11 @@ public:
     Supervisor();
     Supervisor(const Supervisor& orig);
     Supervisor(std::string, std::string, std::string, std::string);
+    virtual std::string toString() override;
     virtual double getCommission() override;
+    virtual double getSales() override;
+    virtual void setCommission(double) override;
+    virtual void setSales(double) override;
     virtual ~Supervisor();
 };
 
